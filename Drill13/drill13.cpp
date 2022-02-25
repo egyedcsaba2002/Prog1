8cf9d1b43d5032b34468aafd079f84bfaededdc4
egyedcsaba2002 <91697497+egyedcsaba2002@users.noreply.github.com>
2022-02-25 10:20:56 +0100

    Add files via upload

---
+++ Drill13/drill13.cpp
@@ -0,0 +1,70 @@
+/*
+    g++ drill13.cpp Graph.cpp Window.cpp GUI.cpp Simple_window.cpp -o main `fltk-config --ldflags --use-images` -std=c++11
+*/
+#include "Simple_window.h"
+#include "Graph.h"
+
+int main()
+{
+    using namespace Graph_lib;
+
+    int xmax = 800;
+    int ymax = 1000;
+
+    Simple_window win {Point{100,100}, xmax, ymax, "Canvas"};
+
+    Lines grid;
+    for (int i = 1; i < 8; ++i)
+    {
+        grid.add(Point{i100,0},Point{i100,800});
+    }
+    for (int i = 1; i < 8; ++i)
+    {
+        grid.add(Point{0,i100},Point{800,i*100});
+    }
+
+    Rectangle rect {Point{0,0}, 100,100};
+    rect.set_fill_color(Color::red);
+    Rectangle rect2 {Point{100,100}, 100,100};
+    rect2.set_fill_color(Color::red);
+    Rectangle rect3 {Point{200,200}, 100,100};
+    rect3.set_fill_color(Color::red);
+    Rectangle rect4 {Point{300,300}, 100,100};
+    rect4.set_fill_color(Color::red);
+    Rectangle rect5 {Point{400,400}, 100,100};
+    rect5.set_fill_color(Color::red);
+    Rectangle rect6 {Point{500,500}, 100,100};
+    rect6.set_fill_color(Color::red);
+    Rectangle rect7 {Point{600,600}, 100,100};
+    rect7.set_fill_color(Color::red);
+    Rectangle rect8 {Point{700,700}, 100,100};
+    rect8.set_fill_color(Color::red);
+
+
+    Imagine imag2{Point{200,0}, "catto2.jpg"};
+    Imagine imag3{Point{0,200}, "catto2.jpg"};
+    Imagine imag4{Point{500,300}, "catto2.jpg"};
+
+    win.attach(imag2);
+    win.attach(imag3);
+    win.attach(imag4);
+
+    win.attach(rect);
+    win.attach(rect2);
+    win.attach(rect3);
+    win.attach(rect4);
+    win.attach(rect5);
+    win.attach(rect6);
+    win.attach(rect7);
+    win.attach(rect8);
+
+    win.attach(grid);
+    // win.wait_for_button();
+
+    for(int i=0;i<8; ++i)
+    {
+        Imagine imag{Point{i*100,i*100}, "catto3.jpg"};
+        win.attach(imag);
+        win.wait_for_button();
+    }
+}
